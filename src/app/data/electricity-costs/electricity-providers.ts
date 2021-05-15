0f1a82833fd9c45effc69c5c7e77feb53ad472e5
Daniel Weiß <wanieldeiss@gmail.com>
2021-05-15 14:13:12 +0200

    add electricity provider

---
+++ src/app/data/electricity-costs/electricity-providers.ts
@@ -0,0 +1,8 @@
+import { ElectricityProvider } from '../electricity-provider.interface';
+
+export default [
+  {
+    name: 'Stadtwerke Landshut',
+    price: 0.279,
+  },
+] as ElectricityProvider[];
---
+++ src/app/data/electricity-provider.interface.ts
@@ -0,0 +1,4 @@
+export interface ElectricityProvider {
+    name: string;
+    price: number;
+}
