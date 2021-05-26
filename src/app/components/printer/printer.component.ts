2d1f87bc07fc8c46b3a43fa68aea2e09465535f4
Daniel Weiß <wanieldeiss@gmail.com>
2021-05-26 06:43:59 +0200

    add printers

--- src/app/components/printer/printer.component.ts
+++ src/app/components/printer/printer.component.ts
@@ -1,24 +1,15 @@
 import { Component } from '@angular/core';
 import { Observable, OperatorFunction } from 'rxjs';
-import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
-import * as Printers  from '../../data/printers'
-
-const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
-  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
-  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
-  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
-  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
-  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { Printer } from 'src/app/data/printer.interface';
+import Printers from '../../data/printers';
 
 @Component({
   selector: 'app-printer',
   templateUrl: './printer.component.html',
-  styleUrls: ['./printer.component.scss']
+  styleUrls: ['./printer.component.scss'],
 })
 export class PrinterComponent {
-  
   constructor() {
     console.log('wat', Printers);
   }
@@ -27,12 +18,16 @@ export class PrinterComponent {
 
   formatter = (result: string) => result.toUpperCase();
 
-  search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+  search: OperatorFunction<string, readonly string[]> = (
+    text$: Observable<string>
+  ) =>
     text$.pipe(
       debounceTime(200),
       distinctUntilChanged(),
-      map(term => term === '' ? []
-        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
-    )
-
+      map((term) =>
+        term === ''
+          ? []
+          : Printers.map((p) => `${p.producer} ${p.model}`).slice(0, 10)
+      )
+    );
 }
