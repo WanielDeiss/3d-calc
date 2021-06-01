f5e19052a330148db3b5ccbe77db0214954009d8
Daniel Weiß <wanieldeiss@gmail.com>
2021-06-01 16:41:34 +0200

    push for windows laptop

--- src/app/components/electricity-provider/electricity-provider.component.ts
+++ src/app/components/electricity-provider/electricity-provider.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, OperatorFunction } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+} from 'rxjs/operators';
+import { ElectricityProvider } from '../../data/electricity-provider.interface';
+import ElectricityProviders from '../../data/electricity-costs/electricity-providers';
 
 @Component({
   selector: 'app-electricity-provider',
   templateUrl: './electricity-provider.component.html',
-  styleUrls: ['./electricity-provider.component.scss']
+  styleUrls: ['./electricity-provider.component.scss'],
 })
-export class ElectricityProviderComponent implements OnInit {
+export class ElectricityProviderComponent {
+  public selectedProvider?: ElectricityProvider;
 
-  constructor() { }
+  formatter = (provider: ElectricityProvider) => provider.name;
 
-  ngOnInit(): void {
-  }
-
+  search: OperatorFunction<string, readonly ElectricityProvider[]> = (
+    text$: Observable<string>
+  ) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      filter((term) => term.length >= 2),
+      map((term) =>
+        ElectricityProviders.filter((provider) =>
+          new RegExp(term, 'mi').test(provider.name)
+        ).slice(0, 10)
+      )
+    );
 }
